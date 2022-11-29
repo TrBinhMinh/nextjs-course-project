512fe08f6a0f7c1823148287599da00c26725342
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-11-29 18:05:30 +0700

    update to lesson 66

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,11 +1,19 @@
 import { useRouter } from "next/router";
 
 function ClientProjectsPage() {
-  const { query } = useRouter();
-  console.log(query);
+  const { query, push } = useRouter();
+
+  const loadProjectHandler = function () {
+    //load data...
+    push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "minh", clientprojectid: "project1" },
+    });
+  };
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
