5dfcde9dadb78fdfbb4ad76d7207d9d7ce8f55ee
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-12-01 18:19:42 +0700

    update to lesson 76

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,23 +1,35 @@
 import Link from "next/link";
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
   return (
-    <li>
-      <img src="" alt="" />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <img src={`/${image}`} alt="" />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <div>
-            <h2>TITLE</h2>
+            <h2>{title}</h2>
           </div>
-          <div>
-            <time>DATE</time>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
           </div>
-          <div>
-            <address>ADDRESS</address>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href="/">Explore Event</Link>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
     </li>
