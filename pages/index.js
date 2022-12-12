2c96920e4ff9944a404b6239aa66dd52b3974a0a
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-12-12 16:30:52 +0700

    update to lesson 142

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
-import { getFeaturedEvents } from "../helpers/api-util";
-import EventList from "../components/events/EventList";
-import Head from "next/head";
-
-function Homepage(props) {
+function HomePage() {
   return (
-    <>
-      <Head>
-        <title>NextJS Events</title>
-        <meta
-          name="description"
-          content="Find a lot of great events that allow you to evolve..."
-        />
-      </Head>
-      <EventList items={props.events} />
-    </>
+    <div>
+      <h1>The Home Page</h1>
+    </div>
   );
 }
 
-export async function getStaticProps() {
-  const events = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: events,
-    },
-    revalidate: 1800,
-  };
-}
-
-export default Homepage;
+export default HomePage;
