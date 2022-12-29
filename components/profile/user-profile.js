6219fc34be96c6f8658b29ab11378050936a500a
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-12-29 15:04:43 +0700

    update to lesson 237

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -20,10 +20,23 @@ function UserProfile() {
 
   // if (isLoading) return <p className={classes.profile}>Loading...</p>;
 
+  async function changPasswordHandler(passwordData) {
+    const res = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changPasswordHandler} />
     </section>
   );
 }
